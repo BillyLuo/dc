d0ee1c8caee398bf7e37858f4556adc90969e481
will <luotao2013@126.com>
2018-04-21 15:40:58 +0800

    headerswiper

--- src/components/home1/headerSwiper.vue
+++ src/components/home1/headerSwiper.vue
@@ -8,17 +8,17 @@
         </div>
         <div class="swiper-slide">
           <div class="hander-swiper-img hander-swiper-2">
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div class="hander-swiper-img hander-swiper-3">
-            <!-- <div class="wrapper">
+            <div class="wrapper">
               <h4>数字资产量化交易平台</h4>
               <h3>Professional Cryptocurrency
                 Exchange Platform</h3>
               <div class="line"></div>
               <div class="understanding">了解详情</div>
-            </div> -->
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="hander-swiper-img hander-swiper-3">
           </div>
         </div>
       </div>
@@ -61,47 +61,47 @@
   }
   .hander-swiper-2{
     background-image: url("/static/img/banner1.png");
-  }
-  .hander-swiper-3{
-    background-image: url("/static/img/banner2.jpg");
-    .wrapper{
-      width: 1200px;
-      margin: 0 auto;
-      padding-top: 160px;
-      color: #fff;
-      text-align: center;
-      font-family: AvertaStdBold,"Microsoft Yahei",arial,Helvetica;
-      h4{
-        font-size: 32px;
-        margin-bottom: 5px;
-      }
-      h3{
-        font-size: 20px;
-        margin-bottom: 20px;
-      }
-      .line{
-        width: 50px;
-        height: 6px;
-        background: #fff;
-        margin: 0 auto 25px auto;
-      }
-      .understanding{
-        height: 35px;
-        padding: 0 25px;
-        line-height: 32px;
-        font-size: 14px;
-        text-align: center;
-        border: 1px solid rgba(255, 255, 255, 0.61);
-        border-radius: 50px;
-        width: 120px;
+      .wrapper{
+        width: 1200px;
         margin: 0 auto;
-        cursor: pointer;
-        &:hover{
+        padding-top: 160px;
+        color: #fff;
+        text-align: center;
+        font-family: AvertaStdBold,"Microsoft Yahei",arial,Helvetica;
+        h4{
+          font-size: 32px;
+          margin-bottom: 5px;
+        }
+        h3{
+          font-size: 20px;
+          margin-bottom: 20px;
+        }
+        .line{
+          width: 50px;
+          height: 6px;
           background: #fff;
-          color: #007aff;
+          margin: 0 auto 25px auto;
+        }
+        .understanding{
+          height: 35px;
+          padding: 0 25px;
+          line-height: 32px;
+          font-size: 14px;
+          text-align: center;
+          border: 1px solid rgba(255, 255, 255, 0.61);
+          border-radius: 50px;
+          width: 120px;
+          margin: 0 auto;
+          cursor: pointer;
+          &:hover{
+            background: #fff;
+            color: #007aff;
+          }
         }
       }
     }
   }
+  .hander-swiper-3{
+    background-image: url("/static/img/banner2.jpg");
 }
 </style>
