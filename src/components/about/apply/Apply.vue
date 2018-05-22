853b269932bc6a22ee9f34932f7543d27853ca57
vic <vic@finchain360.com>
2018-05-22 18:08:49 +0800

    文案

--- src/components/about/apply/Apply.vue
+++ src/components/about/apply/Apply.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <div>
+    <!-- <div>
       <h3>上币申请</h3>
-    </div>
+    </div> -->
+    <apply></apply>
   </div>
 </template>
 
 <script>
+import apply from './apply.md'
 export default {
+    components:{
+        apply
+    },
 
 }
 </script>
---
+++ src/components/about/law/law.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Law></Law>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
+
+<script>
+    import Law from './law.md';
+    export default {
+        components:{
+            Law
+        },
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
